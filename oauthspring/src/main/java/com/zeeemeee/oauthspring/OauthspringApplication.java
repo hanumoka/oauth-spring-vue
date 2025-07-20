b9d657531a4f772caa7888d356496c4855b6abff
HANUYOGA\amagr <amagramer@gmail.com>
2025-07-20 18:04:56 +0900

    1

---
+++ oauthspring/src/main/java/com/zeeemeee/oauthspring/OauthspringApplication.java
@@ -0,0 +1,13 @@
+package com.zeeemeee.oauthspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthspringApplication.class, args);
+    }
+
+}
---
+++ oauthspring/src/test/java/com/zeeemeee/oauthspring/OauthspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeeemeee.oauthspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
