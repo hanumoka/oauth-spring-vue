3e0562a3b9a6339d74e562c9f4be8f679ce5e7e7
HANUYOGA\amagr <amagramer@gmail.com>
2025-07-20 18:18:04 +0900

    spring mysql 연동

---
+++ oauthspring/src/main/java/com/zeeemeee/oauthspring/member/domain/Role.java
@@ -0,0 +1,5 @@
+package com.zeeemeee.oauthspring.member.domain;
+
+public enum Role {
+    ADMIN, USER
+}
---
+++ oauthspring/src/main/java/com/zeeemeee/oauthspring/member/domain/SocialType.java
@@ -0,0 +1,5 @@
+package com.zeeemeee.oauthspring.member.domain;
+
+public enum SocialType {
+    GOOGLE, KAKAO
+}
